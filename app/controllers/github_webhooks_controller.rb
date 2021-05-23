3a04d6b9e7f91e4b5676d58fda3102e2bec76de1
parfenov <parfenov407@gmail.com>
2021-05-23 19:47:02 +0500

    test

--- app/controllers/github_webhooks_controller.rb
+++ app/controllers/github_webhooks_controller.rb
@@ -1,14 +1,15 @@
 class GithubWebhooksController < ActionController::Base
+  skip_before_action :verify_authenticity_token
   include GithubWebhook::Processor
 
   # Handle push event
   def github_push(payload)
-    # TODO: handle push webhook
+    p payload
   end
 
   # Handle create event
   def github_create(payload)
-    # TODO: handle create webhook
+    p payload
   end
 
   private
