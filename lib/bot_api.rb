f8ee4183f4675c738bc6b6e2cf6a6bbf97deb169
parfenov <parfenov407@gmail.com>
2021-10-12 14:58:47 +0500

    fix

--- lib/bot_api.rb
+++ lib/bot_api.rb
@@ -1,7 +1,7 @@
 module Bot
   class API
     def self.notify(type, params)
-      request({"u": "-574891197", "message": get_notification_message(type, params)})
+      request({"u": -574891197, "message": get_notification_message(type, params)})
     end
 
     private
@@ -9,7 +9,8 @@ module Bot
       uri = URI("http://193.176.79.94/bot_notify.php")
 
       Net::HTTP.start(uri.host, uri.port, :use_ssl => false ) do |http|
-        request = Net::HTTP::Post.new(uri.path, {'Content-Type' => 'application/json',  'Authorization' => "Token 8a8abd57da84ca5c20964ea23c28c3cf"})
+        request = Net::HTTP::Post.new(uri.path, 
+          {'Content-Type' => 'application/json',  'Authorization' => "Token 4ea8d6c2aaa2a3fb40958c2ccdc35788"})
         request.form_data = data
         response = http.request request
       end
